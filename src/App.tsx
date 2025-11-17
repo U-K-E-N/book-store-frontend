a2d0efd4ebb0f9a698f9c793f7186f1699ebd644
nastiaserkh <nastiaserkhove@gmail.com>
2025-11-17 20:04:37 +0200

    feat(UKEN-5): add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import BookStoreIcon from './BookStoreIcon/BookStoreIcon';
+import { IconName } from './BookStoreIcon/types';
+
 export const App = () => {
-  return <div>Hello team</div>;
+  return (
+    <div>
+      Hello team
+      {/* Example of icons usage */}
+      <BookStoreIcon
+        iconName={IconName.HeartFilled}
+        count={12}
+      />
+    </div>
+  );
 };
