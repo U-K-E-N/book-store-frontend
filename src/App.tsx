d32772f573eb34a3f7e3764c997f8f1fbdff8856
nastiaserkh <nastiaserkhove@gmail.com>
2025-11-18 18:02:05 +0200

    feat(UKEN-7): Update existing code with variables, move to correct place

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,3 @@
-import BookStoreIcon from './BookStoreIcon/BookStoreIcon';
-import { IconName } from './BookStoreIcon/types';
-
 export const App = () => {
-  return (
-    <div>
-      Hello team
-      {/* Example of icons usage with badge */}
-      <BookStoreIcon
-        iconName={IconName.HeartFilled}
-        count={12}
-      />
-      {/* Example without badge */}
-      <BookStoreIcon iconName={IconName.Search} />
-    </div>
-  );
+  return <div style={{ padding: '20px' }}>Hello team</div>;
 };
