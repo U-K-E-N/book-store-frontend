cde5b2fc764bdef88d54c66a80f6b0b21578694c
Yuliia Kulish <kulish.yulechka@gmail.com>
2025-11-19 19:19:51 +0200

    fix(UKEN-6): fixed input and dropdown

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,17 +6,37 @@ export const Header = () => {
   const [query, setQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('Category');
 
+  const [openInput, setOpenInput] = useState('');
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
+  const handleChangeInputOpen = () => {
+    if (openInput === '') {
+      setOpenInput('is-open');
+    } else {
+      setOpenInput('');
+    }
+  };
+
   return (
     <header>
+      <button
+        type="button"
+        onClick={handleChangeInputOpen}
+      >
+        click
+      </button>
       <Input
+        style={openInput}
         value={query}
         onChange={handleChange}
+        placeholder="Find a book or author"
       />
       <Dropdown
+        //sort, number, category
+        variant="category"
         label="Sort by"
         dropdownText={selectedCategory}
         options={[
