c52484e5ea8e94e5cc5d2c2d38ae3b074a3fbbbf
Karen Behlarian <Gladiatorkaren@gmail.com>
2025-11-20 23:22:24 +0200

    UKEN-21: Implement Hero Slider component

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,22 +1,21 @@
+import { type FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import './Carousel.css';
 
-export const Carousel = () => {
+import './Carousel.scss';
+
+export const Carousel: FC = () => {
   return (
     <div className="carousel-container">
-      {/* Кастомные стрелки */}
-      <div className="custom-prev"></div>
-      <div className="custom-next"></div>
-
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation={{
-          nextEl: '.custom-next',
-          prevEl: '.custom-prev',
+          nextEl: '.carousel-next',
+          prevEl: '.carousel-prev',
         }}
         pagination={{
           el: '.banner-slider__dots',
@@ -27,103 +26,28 @@ export const Carousel = () => {
         className="banner-slider"
       >
         <SwiperSlide>
-          <div className="banner-slider__banner">
-            {/* Основное содержимое слайда */}
-            <div className="banner-slider__main-content">
-              <div className="banner-slider__discount-container">
-                <p className="banner-slider__discount-text">-50%</p>
-              </div>
-
-              <h2 className="banner-slider__title">Constitution day</h2>
-
-              {/* Если нужны дополнительные картинки внутри баннера */}
-              <div className="banner-slider__images">
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/1.jpg"
-                  alt=""
-                />
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/2.jpg"
-                  alt=""
-                />
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/3.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
+          <div className="banner-slider__banner banner-slider__banner--first">
+            <div className="banner-slider__main-content"></div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className="banner-slider__banner">
-            {/* Основное содержимое слайда */}
-            <div className="banner-slider__main-content">
-              <div className="banner-slider__discount-container">
-                <p className="banner-slider__discount-text">-50%</p>
-              </div>
-
-              <h2 className="banner-slider__title">Constitution day</h2>
-
-              {/* Если нужны дополнительные картинки внутри баннера */}
-              <div className="banner-slider__images">
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/1.jpg"
-                  alt=""
-                />
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/2.jpg"
-                  alt=""
-                />
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/3.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
+          <div className="banner-slider__banner banner-slider__banner--second">
+            <div className="banner-slider__main-content"></div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className="banner-slider__banner">
-            {/* Основное содержимое слайда */}
-            <div className="banner-slider__main-content">
-              <div className="banner-slider__discount-container">
-                <p className="banner-slider__discount-text">-50%</p>
-              </div>
-
-              <h2 className="banner-slider__title">Constitution day</h2>
-
-              {/* Если нужны дополнительные картинки внутри баннера */}
-              <div className="banner-slider__images">
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/1.jpg"
-                  alt=""
-                />
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/2.jpg"
-                  alt=""
-                />
-                <img
-                  className="banner-slider__image"
-                  src="/books/img/hero/hero_small_images/3.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
+          <div className="banner-slider__banner banner-slider__banner--third">
+            <div className="banner-slider__main-content"></div>
           </div>
         </SwiperSlide>
       </Swiper>
 
-      <div className="banner-slider__dots"></div>
+      <div className="carousel-prev"></div>
+      <div className="carousel-next"></div>
+
+      <div className="banner-slider__dots" />
     </div>
   );
 };
